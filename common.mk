1c4315d608f2ea5ececb57e92cf8f44c65b63227
Dillon Brosche <dillonbrosche@gmail.com>
2020-11-07 16:07:11 +0100

    sm8150-common: Update ims privapp permissions
    * I'm using CAF ims 43036ad4756c1e7471ccfcd39dacbe82dd892d2c
    
    Change-Id: I922e562286f7859d551821a64620d2b1aafcafed
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>

--- common.mk
+++ common.mk
@@ -462,7 +462,7 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-ims.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-ims.xml
 
 # TextClassifier
 PRODUCT_PACKAGES += \
