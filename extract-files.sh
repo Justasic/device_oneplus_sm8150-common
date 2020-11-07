b138f605b841c1991f729ea3d95988467ba4be68
Filippo <zzkw35@gmail.com>
2020-11-07 15:32:09 +0100

    sm8150-common: sed vendor.qti.hardware.data.connection
    * I'd rather let extract-files script do all the work instead of pinning/manually editing the files each time I update vendor.
    
    Change-Id: I023981cb4075b0d0030343db339792b306428eb8
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>

--- extract-files.sh
+++ extract-files.sh
@@ -41,6 +41,26 @@ function blob_fixup() {
     etc/permissions/qti_libpermissions.xml)
         sed -i -e 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
     ;;
+    vendor/lib/hw/camera.qcom.so | vendor/lib64/hw/camera.qcom.so)
+        sed -i "s/libhidltransport.so/qtimutex.so\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
+        ;;
+
+    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml)
+        sed -i -e 's|xml version=\"2.0\"|xml version=\"1.0\"|g' "${2}"
+        ;;
+
+    product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+        sed -i -e 's|xml version=\"2.0\"|xml version=\"1.0\"|g' "${2}"
+        ;;
+    esac
+    case "${DEVICE}" in
+        hotdogg )
+        case "${1}" in
+            vendor/lib/libgf_ud_hal.so | vendor/lib64/libgf_ud_hal.so )
+            sed -i "s|vendor.boot.verifiedbootstate|vendor.boot.fingerprintbstate|g" "${2}"
+            ;;
+            esac
+    ;;
     esac
 }
 
