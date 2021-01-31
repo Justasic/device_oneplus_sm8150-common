5cf9e56df334eff31b953007518b3fed300f4280
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-01-31 22:14:34 +0530

    sm8150-common: switch to standard lights hal
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>

--- common.mk
+++ common.mk
@@ -360,7 +360,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.oneplus_msmnile
+    android.hardware.lights-service.qti
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
